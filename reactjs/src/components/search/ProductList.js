39c2ca21b51fae8c0b4fb3237ddd08ca675f75bf
MayurBhowad <mayurbhowad1995@gmail.com>
2021-05-29 16:40:57 +0530

    edit: search query

--- reactjs/src/components/search/ProductList.js
+++ reactjs/src/components/search/ProductList.js
@@ -12,7 +12,7 @@ function ProductList({ productList = [] }) {
                                 <>
                                     <div key={item.id} className="item">
                                         <h4>{item.product_name}</h4><br />
-                                        <p>Available in {item.store_count} store nearby at avg cost of ${item.price}</p>
+                                        <p>Available in {item.store_count} store nearby at avg cost of ${item.price.toFixed(2)}</p>
                                     </div><hr />
                                 </>
                             ))}
